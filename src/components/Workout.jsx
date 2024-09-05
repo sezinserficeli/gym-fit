eb33d24057011422eb65cafbcb07c74d7b548602
Sezin Çağla Serficeli <serficelis@gmail.com>
2024-09-05 15:48:37 +0300

    initial commit

---
+++ src/components/Workout.jsx
@@ -0,0 +1,18 @@
+import ExerciseCard from "./ExerciseCard"
+import SectionWrapper from "./SectionWrapper"
+
+export default function Workout(props) {
+  const { workout } = props
+
+  return (
+    <SectionWrapper id={'workout'} header={"Welcome to"} title={['The', 'DANGER', 'zone']}>
+      <div className="flex flex-col gap-4">
+        {workout.map((exercise, i) => {
+          return (
+            <ExerciseCard i={i} exercise={exercise} key={i} />
+          )
+        })}
+      </div>
+    </SectionWrapper>
+  )
+}
